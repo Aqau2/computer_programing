cf4f61fff92f954b8d82e27f7b1b65a8e1836ec7
2018003kimdaeun <kidang01@naver.com>
2023-09-08 20:45:34 +0900

    숫자입력받기 고침
    안내문구 변경

--- test1.py
+++ test1.py
@@ -1,5 +1,5 @@
-name = input()
-number_one = input()
-number_two = input()
+name = input("이름을 입력하세여:")
+number_one = input("첫번뺴 숫자를 입력:")
+number_two = input("두번쨰 숫자를 입력")
 
 print("안녕하세요" +name+ "님. 입력하신 두 숫자는" +number_one+ "와" +number_two+ "입니다")
